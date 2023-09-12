15e83f7bc31bf4fffa307af7da8d4073139b79d5
Elizabeth <tjeudy.e@gmail.com>
2023-09-12 11:35:23 -0400

    Initial commit

---
+++ server/src/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+import { userRouter } from "./routes/users.js";
+
+// generates api
+const app = express();
+
+// middleware
+app.use(express.json()); // converts frontend data to json
+app.use(cors()); // helps with frontend errors
+app.use("/auth", userRouter);
+mongoose.connect(
+	"mongodb+srv://lizzy:wVkBElAoPdsMcu1k@recipes.yinr1jx.mongodb.net/recipes?retryWrites=true&w=majority"
+); // generates connection to server
+
+app.listen(3002, () => console.log("SERVER STARTED")); // tells api to start
---
+++ server/src/routes/users.js
@@ -0,0 +1,44 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { UserModel } from "../models/Users.js";
+
+const router = express.Router();
+
+// register route, request gets data from user making request to endpoint;
+// response sends data make to user making api request
+router.post("/register", async (req, res) => {
+	const { username, password } = req.body;
+	// waits for username match
+	const user = await UserModel.findOne({ username: username });
+	if (user) {
+		return res.json({ message: "User already exists!" });
+	}
+
+	// bcrypt for password security
+	const hashedPassword = await bcrypt.hash(password, 10);
+
+	const newUser = new UserModel({ username, password: hashedPassword });
+	await newUser.save();
+	res.json({ message: "Congrats, you've successfully created your account!" });
+});
+
+// login route
+router.post("/login", async (req, res) => {
+	const { username, password } = req.body;
+	const user = await UserModel.findOne({ username: username });
+	if (!user) {
+		return res.json({ message: "User Doesn't Exist!" });
+	}
+	// password matches one in database
+	const isPasswordValid = await bcrypt.compare(password, user.password);
+	if (!isPasswordValid) {
+		return res.json({ message: "Username or password is incorrect!" });
+	}
+
+	// if correct; secret verifies authentication
+	const token = jwt.sign({ id: user._id }, "secret");
+	res.json({ token, userID: user._id });
+});
+
+export { router as userRouter };
